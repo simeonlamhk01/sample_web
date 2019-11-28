2f081dee4a4b5932150179d8fa4308481114c513
Simeon Lam <simeonlam@hk01.com>
2019-11-28 12:12:41 +0800

    feat: test logger feature

--- src/server.js
+++ src/server.js
@@ -1,5 +1,41 @@
+function Logger(...args) {
+  this.args = args;
+}
+Logger.prototype.info = function (msg) { msg.pod_name = "myLogger"; console.log(msg); };
+Logger.prototype.error = function (msg) { msg.pod_name = "myLogger"; console.log(msg); };
+Logger.prototype.debug = function (msg) { msg.pod_name = "myLogger"; console.log(msg); };
+Logger.prototype.fatal = function (msg) { msg.pod_name = "myLogger"; console.log(msg); };
+Logger.prototype.warn = function (msg) { msg.pod_name = "myLogger"; console.log(msg);};
+Logger.prototype.trace = function (msg) { msg.pod_name = "myLogger"; console.log(msg); };
+Logger.prototype.child = function () { return new Logger() };
+
+
+const myLogger = new Logger()
+
 const fastify = require('fastify')({
-  logger: { level: 'debug' }
+  logger: {
+    level: 'trace',
+    serializers: {
+      req: function asReqValue (req) {
+        return {
+          method: req.method,
+          url: req.url,
+          version: req.headers['accept-version'],
+          hostname: req.hostname,
+          remoteAddress: req.ip,
+          remotePort: req.connection.remotePort,
+          podName: 'test pod name'
+        }
+      },
+      res: function asResValue (res) {
+        return {
+          statusCode: res.statusCode,
+          podName: 'test pod name'
+        }
+      }
+    }
+  }
+  // logger: myLogger
 })
 const statusPlugin = require('fastify-status')
 
@@ -9,11 +45,44 @@ fastify.register(statusPlugin, {
   hide: true
 })
 
+const sleep = (s) => {
+  return new Promise(resolve => setTimeout(resolve, s*1000));
+}
+
+const complete = []
+
+fastify.post('/test', async (req, reply) => {
+  const body = req.body
+  console.log(JSON.stringify(body, null, 2))
+  reply.code(200).send({ url: `https://66c40410.ngrok.io/test?id=${body.id}`, message: 'start' })
+  await sleep(30)
+  complete.push(body.id)
+  console.log(`added: ${body.id}`)
+})
+
+fastify.get('/test', async (req, reply) => {
+  const query = req.query
+
+  console.log(JSON.stringify(complete, null, 2))
+
+  let status = { status: 'Success'}
+  if (complete.indexOf(query.id) === -1)
+    status = { status: 'Not complete'}
+  else
+    complete.pop()
+  reply.code(200).send(status)
+})
+
 // ---- Run the server! ----
 const start = async () => {
   try {
     await fastify.listen(3000, '0.0.0.0')
     fastify.log.info(`server listening on ${fastify.server.address().port}`)
+    fastify.log.trace(`trace`)
+    fastify.log.warn(`warn`)
+    fastify.log.debug(`debug`)
+    fastify.log.error(`error`)
+    fastify.log.fatal(`fatal`)
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
